2d4e564842d5d9afeead6a82d77934cd5a914718
koreander2001 <neokamiyama@gmail.com>
2021-09-25 16:33:52 +0900

    リスト項目や追加ボタンをタップ時処理実装の布石として、タップ時にトーストを表示するよう変更

--- lib/pages/note_list_page.dart
+++ lib/pages/note_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:listnote/main.dart';
 
@@ -16,25 +17,25 @@ class NoteListPage extends HookWidget {
       home: Scaffold(
         appBar: AppBar(title: Text(title)),
         body: ListView(children: noteList.map(_messageItem).toList()),
-        floatingActionButton: const FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: null,
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => Fluttertoast.showToast(msg: 'Add button'),
         ),
       ),
     );
   }
 
-  Widget _messageItem(String title) {
+  Widget _messageItem(String message) {
     return Container(
       decoration: const BoxDecoration(
         border: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
       ),
       child: ListTile(
         title: Text(
-          title,
+          message,
           style: const TextStyle(color: Colors.black, fontSize: 18.0),
         ),
-        onTap: null,
+        onTap: () => Fluttertoast.showToast(msg: message),
       ),
     );
   }
